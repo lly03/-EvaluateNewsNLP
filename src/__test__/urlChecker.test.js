86ddce9c2e6622a352a2fa6c33f6598fb89fd02e
Lucinda Ly <lucindaly03@gmail.com>
2021-10-04 20:37:45 +1100

    update urlChecker test file

--- src/__test__/urlChecker.test.js
+++ src/__test__/urlChecker.test.js
@@ -1,5 +1,5 @@
 // Import the js file to test
-import { urlChecker } from "./client/js/urlChecker"
+import { urlChecker } from "../client/js/urlChecker"
 
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
